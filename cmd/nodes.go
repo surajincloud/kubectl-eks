b2d6251fb16bf10170f8998364723be609a90c97
surajnarwade <suraj.narwade@rvu.co.uk>
2023-02-17 16:40:14 +0000

    improve nodes command and add addon command

--- cmd/nodes.go
+++ cmd/nodes.go
@@ -1,11 +1,17 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
 	"text/tabwriter"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
 	"github.com/spf13/cobra"
 	"github.com/surajincloud/kubectl-eks/pkg/kube"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -41,14 +47,36 @@ func nodes(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
+	ctx := context.Background()
+	region := "eu-west-1"
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
+	if err != nil {
+		log.Fatal(err)
+	}
+	// client := eks.NewFromConfig(cfg)
+	client2 := ec2.NewFromConfig(cfg)
+
 	w := tabwriter.NewWriter(os.Stdout, 5, 2, 3, ' ', tabwriter.TabIndent)
 	defer w.Flush()
-	fmt.Fprintln(w, "NAME", "\t", "ARCH", "\t", "INSTANCE-TYPE", "\t", "OS", "\t", "CAPACITY-TYPE", "\t", "REGION", "\t", "AGE")
+	fmt.Fprintln(w, "NAME", "\t", "INSTANCE-TYPE", "\t", "OS", "\t", "CAPACITY-TYPE", "\t", "REGION", "\t", "AMI-ID", "\t", "AMI-NAME", "\t", "AGE")
 	for _, i := range nodeList {
 		age := getAge(i.CreationTimestamp)
-		fmt.Fprintln(w, i.Name, "\t", i.Labels[kube.ArchLabel], "\t", i.Labels[kube.InstanceTypeLabel], "\t", i.Labels[kube.OsLabel], "\t", i.Labels[kube.CapacityTypeLabel], "\t", i.Labels[kube.ZoneLabel], "\t", age)
-	}
+		img := i.Labels[kube.NodeGroupImage]
+		dis, _ := client2.DescribeImages(ctx, &ec2.DescribeImagesInput{ImageIds: []string{img}})
 
+		amiName := aws.ToString(dis.Images[0].Name)
+		fmt.Fprintln(w, i.Name, "\t", i.Labels[kube.InstanceTypeLabel], "\t", i.Labels[kube.OsLabel], "\t", i.Labels[kube.CapacityTypeLabel], "\t", i.Labels[kube.ZoneLabel], "\t", i.Labels[kube.NodeGroupImage], "\t", amiName, "\t", age)
+	}
+	// SSM parameter
+	//aws ssm get-parameter --name /aws/service/eks/optimized-ami/1.24/amazon-linux-2/recommended/image_id --region region-code --query "Parameter.Value" --output text
+	client3 := ssm.NewFromConfig(cfg)
+	out, err := client3.GetParameter(ctx, &ssm.GetParameterInput{
+		Name: aws.String("/aws/service/eks/optimized-ami/1.23/amazon-linux-2/recommended/image_id"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(aws.ToString(out.Parameter.Value))
 	return nil
 }
 
