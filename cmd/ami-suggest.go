c749de3ca697c005698a7a8359c7507ce6ac4525
Suraj Narwade <surajnarwade353@gmail.com>
2023-06-06 21:47:32 +0100

    fix ami & kubeconfig command

--- cmd/ami-suggest.go
+++ cmd/ami-suggest.go
@@ -52,23 +52,30 @@ func suggestion(cmd *cobra.Command, args []string) error {
 	descluster, _ := client2.DescribeCluster(ctx, &eks.DescribeClusterInput{
 		Name: aws.String(clusterName),
 	})
-	fmt.Printf("Cluster name: %v\n", clusterName)
-	fmt.Printf("Cluster version: %v\n", aws.ToString(descluster.Cluster.Version))
 	clusterVersion := aws.ToString(descluster.Cluster.Version)
 	client3 := ssm.NewFromConfig(cfg)
+	fmt.Println("Recommended versions for:")
+	fmt.Println("Amazon Linux 2:")
 	out, err := client3.GetParameter(ctx, &ssm.GetParameterInput{
 		Name: aws.String(fmt.Sprintf("/aws/service/eks/optimized-ami/%s/amazon-linux-2/recommended/release_version", clusterVersion))})
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Recommended Release version for EKS optimised amazon linux AMI: %v\n", aws.ToString(out.Parameter.Value))
+	fmt.Printf("Release version: %v\n", aws.ToString(out.Parameter.Value))
 
 	out, err = client3.GetParameter(ctx, &ssm.GetParameterInput{
 		Name: aws.String(fmt.Sprintf("/aws/service/eks/optimized-ami/%s/amazon-linux-2/recommended/image_id", clusterVersion))})
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Recommended Image ID for EKS optimised amazon linux AMI: %v\n", aws.ToString(out.Parameter.Value))
+	fmt.Printf("Image ID: %v\n", aws.ToString(out.Parameter.Value))
+	fmt.Println("Bottlerocket:")
+	out, err = client3.GetParameter(ctx, &ssm.GetParameterInput{
+		Name: aws.String(fmt.Sprintf("/aws/service/bottlerocket/aws-k8s-%s/x86_64/latest/image_id", clusterVersion))})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Image ID: %v\n", aws.ToString(out.Parameter.Value))
 
 	return nil
 
