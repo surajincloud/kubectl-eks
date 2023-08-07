e1a2e8ebd4903abdc821e56a81566db1eacab3a7
Justin Garrison <justinleegarrison@gmail.com>
2023-08-07 08:37:04 -0700

    Add logs subcommand (#71)
    * Make ssm subcommand require node argument
    
    * Add cloudwatch log streams to logs subcommand
    
    * Add node logging capabilities
    
    * Implement crude follow for cloudwatch
    
    * Add node query for files, follow, and multiple services
    
    * Working follow and multi query for CWL
    
    * Remove exec kubectl calls
    
    * Close node logs channel
    
    * Add extra checks for control plane logging
    
    * Add function comments and remove unused code
    
    * remove unused copyright banner

---
+++ cmd/logs.go
@@ -0,0 +1,460 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/antchfx/jsonquery"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
+	"github.com/aws/aws-sdk-go-v2/service/eks"
+	"github.com/markusmobius/go-dateparser"
+	"github.com/spf13/cobra"
+	awspkg "github.com/surajincloud/kubectl-eks/pkg/aws"
+	"github.com/surajincloud/kubectl-eks/pkg/kube"
+)
+
+var Follow bool
+var SinceTime string
+
+func init() {
+	logsCmd.Flags().BoolVarP(&Follow, "follow", "f", false, "Follow logs (not available for node file queries)")
+	logsCmd.Flags().StringVar(&SinceTime, "since", "1 hour ago", "What time logs should start from")
+}
+
+// logsCmd represents the logs command
+var logsCmd = &cobra.Command{
+	Use: "logs [flags] LOG_SOURCE",
+	Example: `    kubectl eks logs kube-apiserver
+    kubectl eks logs NODE [kubelet]
+  
+  Query multiple log sources:
+    kubectl eks logs api audit scheduler
+    kubectl eks logs NODE kubelet containerd`,
+	Args:  cobra.MinimumNArgs(1),
+	Short: "Get logs from EKS control plane or nodes",
+	Long:  "Get logs from EKS control plane or nodes",
+	RunE:  logs,
+}
+
+// the main logs function is responsible for routing the request
+// to the correct log endpoint (cloudwatch or kubelet) and
+// printing the logs to stdout from the logs channel
+func logs(cmd *cobra.Command, args []string) error {
+
+	// pass empty string to let fuction get name
+	clusterName, err := kube.GetClusterName(*KubernetesConfigFlags.ClusterName)
+	if err != nil {
+		return err
+	}
+
+	// we only look at the first argument to determine if it is a control plane log source or node
+	logTarget := args[0]
+	logsChan := make(chan string)
+	logsDoneChan := make(chan bool, len(args))
+
+	cloudwatchLogStreams := []string{
+		"scheduler",
+		"kube-scheduler",
+		"audit",
+		"kube-audit",
+		"kube-apiserver-audit",
+		"cm",
+		"controller-manager",
+		"kube-controller-manager",
+		"api",
+		"apiserver",
+		"kube-apiserver",
+		"auth",
+		"authenticator",
+		"ccm",
+		"cloud-controller",
+		"cloud-controller-manager",
+	}
+
+	// handle ctl+c interrupt without printing output
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		os.Exit(0)
+	}()
+
+	// check first argument if it is a control plane log source or node
+	if contains(cloudwatchLogStreams, logTarget) || (logTarget == "all") {
+
+		if logTarget == "all" {
+			args = args[1:]
+			args = append(args, "scheduler", "kube-apiserver-audit", "kube-controller-manager", "kube-apiserver", "authenticator", "cloud-controller-manager")
+		}
+
+		var cwlStreamInput cloudwatchlogs.DescribeLogStreamsInput
+		var streams *cloudwatchlogs.DescribeLogStreamsOutput
+		var limit int32 = 100
+		var cwlGroupPrefix string
+
+		cwlGroupName := "/aws/eks/" + clusterName + "/cluster"
+		cwlStreamInput.LogGroupName = &cwlGroupName
+
+		// aws config
+		ctx := context.Background()
+		// read flag values
+		region, _ := cmd.Flags().GetString("region")
+
+		cfg, err := awspkg.GetAWSConfig(ctx, region)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// TODO check if logging is enabled
+		// TODO allow user to enable logging
+		svc := eks.NewFromConfig(cfg)
+		eksClusterInput := &eks.DescribeClusterInput{
+			Name: aws.String(clusterName),
+		}
+		result, _ := svc.DescribeCluster(ctx, eksClusterInput)
+		// check if logging is enabled
+		if !*result.Cluster.Logging.ClusterLogging[0].Enabled == true {
+			fmt.Println("Logging is not enabled for this cluster. Please enable logging and try again.")
+			os.Exit(1)
+		}
+
+		cwl := cloudwatchlogs.NewFromConfig(cfg)
+		// verify the group exists first
+		err = ensureLogGroupExists(cwlGroupName, ctx, cwl)
+		//TODO prompt if user wants to enable logs
+		if err != nil {
+			panic(err)
+		}
+
+		var fetchStream string
+		matchedStream := 0
+		for _, logSource := range args {
+
+			cwlGroupPrefix = getLogStreamPrefix(logSource)
+			cwlStreamInput.LogStreamNamePrefix = &cwlGroupPrefix
+
+			streams, err = cwl.DescribeLogStreams(ctx, &cwlStreamInput)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if len(streams.LogStreams) == 0 {
+				fmt.Fprintln(os.Stderr, "No log streams found for", cwlGroupPrefix)
+			} else if cwlGroupPrefix == "kube-apiserver" {
+				// we have to make sure kube-apiserver doesn't add kube-apiserver-audit logs
+				for _, stream := range streams.LogStreams {
+					if !strings.Contains(*stream.LogStreamName, "kube-apiserver-audit") {
+						fetchStream = *stream.LogStreamName
+						// only match the first stream
+						break
+					}
+				}
+				if fetchStream == "" {
+					fmt.Fprintln(os.Stderr, "No log streams found for", cwlGroupPrefix)
+				} else {
+					go getLogEvents(&cwlGroupName, &fetchStream, &limit, logsChan, logsDoneChan, ctx, cwl, len(args))
+					matchedStream++
+
+				}
+			} else {
+				fetchStream = *streams.LogStreams[0].LogStreamName
+				go getLogEvents(&cwlGroupName, &fetchStream, &limit, logsChan, logsDoneChan, ctx, cwl, len(args))
+				matchedStream++
+			}
+		}
+
+		// use a count to make sure we have a goroutine fetching logs
+		if matchedStream > 0 {
+			// Print each line from logsChan
+			for log := range logsChan {
+				// print the log
+				fmt.Println(log)
+
+				// once all event streams are done, close the channel
+				if len(logsDoneChan) == len(args) {
+					close(logsChan)
+				}
+			}
+		}
+	} else {
+		// we assume the target is a node instead of control plane
+		nodeList, err := kube.GetNodes(KubernetesConfigFlags)
+		if err != nil {
+			return err
+		}
+
+		var nodeMatched bool = false
+		var currentNodeSlice []string
+		logTargetSlice := strings.Split(logTarget, ".")
+
+		for _, i := range nodeList {
+			// match node based on substring eg. ip-192-168-1-1
+			currentNodeSlice = strings.Split(i.Name, ".")
+			if currentNodeSlice[0] == logTargetSlice[0] {
+				nodeMatched = true
+				var query []string
+
+				// use all additional arguments as services to query
+				if len(args) > 1 {
+					query = args[1:]
+				} else {
+					query = append(query, "kubelet")
+				}
+
+				// validate kubelet settings for remote logs
+				if validateKubeletConfig(i.Name) {
+					// get logs and assume query is journald and can accept sinceTime
+					go getNodeLogs(i.Name, query, false, logsChan, logsDoneChan)
+
+					// print each line from logsChan
+					for log := range logsChan {
+
+						fmt.Println(log)
+
+						if len(logsDoneChan) == 1 {
+							close(logsChan)
+						}
+					}
+				}
+			}
+		}
+		if nodeMatched {
+			return nil
+		} else {
+			fmt.Printf("Node %s not found\nTo query control plane logs please see options in --help output.\n", logTarget)
+		}
+	}
+	return nil
+}
+
+// fetches logs from a node using the kubelet api
+// sends logs to the logsChan and sends a bool to logsDoneChan when done
+// there are additional queries that can be used on the kubelet API but
+// I wanted to keep the UX simple so people should rely on CLI tools
+// to filter logs further
+func getNodeLogs(node string, query []string, fileQuery bool, logsChan chan<- string, logsDoneChan chan<- bool) {
+
+	dt, err := dateparser.Parse(nil, SinceTime)
+	if err != nil {
+		panic(err)
+	}
+	// create URL for log fetching
+	rawURL := "/api/v1/nodes/" + node + "/proxy/logs/"
+
+	for {
+
+		clientSet, _ := kube.ClientSet(KubernetesConfigFlags)
+		req := clientSet.RESTClient().Get().
+			AbsPath(rawURL)
+
+		for _, q := range query {
+			req.Param("query", q)
+		}
+
+		if !fileQuery {
+			// sinceTime is ignored for file queries
+			req.Param("sinceTime", dt.Time.Format(time.RFC3339))
+		}
+
+		resp, err := req.DoRaw(context.Background())
+		if err != nil {
+			log.Panicln(err, req.URL().String())
+		}
+
+		// api returns a byte string
+		// convert to string and split by newline to send each line to channel
+		for _, logLine := range strings.Split(string(resp), "\n") {
+			// fmt.Printf("%v %s", lineNumber, logLine)
+			if strings.Contains(logLine, "options present and query resolved to log files") {
+				// file queries cannot use sinceTime
+				// we catch this error output and run again as file query
+				go getNodeLogs(node, query, true, logsChan, logsDoneChan)
+				break
+			} else if logLine == "" ||
+				strings.Contains(logLine, "-- No entries --") ||
+				strings.Contains(logLine, "-- Logs begin at ") {
+				// don't send log decorations
+			} else {
+				logsChan <- logLine
+			}
+		}
+
+		if Follow {
+			if fileQuery {
+				fmt.Fprintln(os.Stderr, "Cannot follow file queries")
+				close(logsChan)
+				break
+			}
+			dt, err = dateparser.Parse(nil, "now")
+			if err != nil {
+				panic(err)
+			}
+			time.Sleep(1 * time.Second)
+		} else {
+			if fileQuery {
+				close(logsChan)
+			} else {
+				logsDoneChan <- true
+			}
+			break
+		}
+	}
+}
+
+// ensureLogGroupExists first checks if the log group exists
+// Cluster logging can be enabled but the log group may not exist
+// this is because not all control plane components need logging enabled
+func ensureLogGroupExists(name string, ctx context.Context, cwl *cloudwatchlogs.Client) error {
+	resp, err := cwl.DescribeLogGroups(ctx, &cloudwatchlogs.DescribeLogGroupsInput{})
+	if err != nil {
+		return err
+	}
+
+	for _, logGroup := range resp.LogGroups {
+		if *logGroup.LogGroupName == name {
+			return nil
+		}
+	}
+
+	return err
+}
+
+// fetchLogs fetches logs from a cloudwatch stream
+// sends logs to the logsChan and sends true to logsDoneChan when done
+// logs are not guaranteed to be in order because they are fetched from each source
+// individually and no ordering is performed on the channel before printing
+func getLogEvents(logGroupName *string, logStreamName *string, limit *int32, logsChan chan<- string, logsDoneChan chan<- bool, ctx context.Context, cwl *cloudwatchlogs.Client, totalStreams int) {
+
+	dt, err := dateparser.Parse(nil, SinceTime)
+	if err != nil {
+		panic(err)
+	}
+
+	// loop forever if Follow == true
+	for {
+		resp, err := cwl.GetLogEvents(ctx, &cloudwatchlogs.GetLogEventsInput{
+			Limit:         limit,
+			LogGroupName:  logGroupName,
+			LogStreamName: logStreamName,
+			StartTime:     aws.Int64(dt.Time.UnixMilli()),
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		for i, event := range resp.Events {
+			// TODO allow for following tokens for more logs from different streams
+			// currently we only fetch the newest stream which may not have all logs
+
+			if i == len(resp.Events)-1 {
+				if Follow {
+					dt, err = dateparser.Parse(nil, "now")
+					if err != nil {
+						panic(err)
+					}
+					// wait 1 sec before querying again
+					time.Sleep(1 * time.Second)
+				} else {
+					logsDoneChan <- true
+				}
+				logsChan <- *event.Message
+			} else {
+				logsChan <- *event.Message
+			}
+
+		}
+
+	}
+}
+
+// initialize command and get region
+func init() {
+	rootCmd.AddCommand(logsCmd)
+	logsCmd.PersistentFlags().String("region", "", "region")
+}
+
+// check if a string is present in a slice
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Convert possible log source aliases to full log stream prefix
+// needed because log streams have defined prefixes but we allow
+// multiple aliases for each log source
+func getLogStreamPrefix(logTarget string) string {
+	schedulerSlice := []string{"scheduler", "kube-scheduler"}
+	auditSlice := []string{"audit", "kube-audit", "kube-apiserver-audit"}
+	cmSlice := []string{"cm", "controller-manager", "kube-controller-manager"}
+	apiSlice := []string{"api", "apiserver", "kube-apiserver"}
+	authSlice := []string{"auth", "authenticator"}
+	ccmSlice := []string{"ccm", "cloud-controller", "cloud-controller-manager"}
+
+	if contains(schedulerSlice, logTarget) {
+		return "kube-scheduler"
+	} else if contains(auditSlice, logTarget) {
+		return "kube-apiserver-audit"
+	} else if contains(cmSlice, logTarget) {
+		return "kube-controller-manager"
+	} else if contains(apiSlice, logTarget) {
+		return "kube-apiserver"
+	} else if contains(authSlice, logTarget) {
+		return "authenticator"
+	} else if contains(ccmSlice, logTarget) {
+		return "cloud-controller-manager"
+	} else {
+		return ""
+	}
+}
+
+// validates kubelet config for remote logging
+// fetches the full config from the configz endpoint
+// verifies if the settings are set correctly
+// there's currently no way I know to change this without restarting the kubelet
+func validateKubeletConfig(node string) bool {
+	rawURL := "/api/v1/nodes/" + node + "/proxy/configz"
+	clientSet, _ := kube.ClientSet(KubernetesConfigFlags)
+	req := clientSet.RESTClient().Get().
+		AbsPath(rawURL).Timeout(20 * time.Second)
+
+	resp, err := req.DoRaw(context.Background())
+	if err != nil {
+		log.Panicln(err, req.URL().String())
+	}
+
+	// read the kueblet config json
+	kubeletConfigJson, err := jsonquery.Parse(strings.NewReader(string(resp)))
+	if err != nil {
+		panic(err)
+	}
+
+	// check if the node has the appropriate config for remote logging
+	nodeLogQuery := jsonquery.FindOne(kubeletConfigJson, "kubeletconfig/featureGates/NodeLogQuery")
+
+	systemLogHandler := jsonquery.FindOne(kubeletConfigJson, "kubeletconfig/enableSystemLogHandler")
+	systemLogQuery := jsonquery.FindOne(kubeletConfigJson, "kubeletconfig/enableSystemLogQuery")
+	if (nodeLogQuery != nil) && (systemLogHandler != nil) && (systemLogQuery != nil) {
+		if nodeLogQuery.Value().(bool) {
+			return true
+		}
+	}
+	// if the node doesn't have the appropriate config, print a message
+	fmt.Printf(`	Node %s is not configured for remote logs.
+	Please enable remote logging on the kubelet from the documentation here
+	Requires Kubernetes 1.27 https://kubernetes.io/blog/2023/04/21/node-log-query-alpha/`, node)
+
+	return false
+
+}
--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/surajincloud/kubectl-eks
 go 1.20
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.17.4
+	github.com/antchfx/jsonquery v1.3.2
+	github.com/aws/aws-sdk-go-v2 v1.19.0
 	github.com/aws/aws-sdk-go-v2/config v1.18.13
+	github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs v1.22.1
 	github.com/aws/aws-sdk-go-v2/service/ec2 v1.64.0
 	github.com/aws/aws-sdk-go-v2/service/eks v1.27.3
 	github.com/aws/aws-sdk-go-v2/service/ssm v1.31.3
+	github.com/markusmobius/go-dateparser v1.2.1
 	github.com/mmmorris1975/ssm-session-client v0.400.1
 	github.com/siderolabs/go-kubeconfig v0.1.0
 	github.com/spf13/cobra v1.6.1
@@ -18,11 +21,12 @@ require (
 )
 
 require (
+	github.com/antchfx/xpath v1.2.3 // indirect
 	github.com/aws/aws-sdk-go v1.44.76 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.13.13 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.22 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.28 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.22 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.35 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.29 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.29 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.22 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.12.2 // indirect
@@ -33,6 +37,7 @@ require (
 	github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
+	github.com/elliotchance/pie/v2 v2.7.0 // indirect
 	github.com/emicklei/go-restful/v3 v3.9.0 // indirect
 	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
@@ -42,6 +47,7 @@ require (
 	github.com/go-openapi/jsonreference v0.20.0 // indirect
 	github.com/go-openapi/swag v0.19.14 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/btree v1.0.1 // indirect
 	github.com/google/gnostic v0.5.7-v3refs // indirect
@@ -51,12 +57,16 @@ require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/gregjones/httpcache v0.0.0-20180305231024-9cad4c3443a7 // indirect
+	github.com/hablullah/go-hijri v1.0.2 // indirect
+	github.com/hablullah/go-juliandays v1.0.0 // indirect
 	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/jalaali/go-jalaali v0.0.0-20210801064154-80525e88d958 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/liggitt/tabwriter v0.0.0-20181228230101-89fcab3d43de // indirect
+	github.com/magefile/mage v1.15.0 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
@@ -68,17 +78,20 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
+	github.com/tetratelabs/wazero v1.3.1 // indirect
 	github.com/twinj/uuid v0.0.0-20151029044442-89173bcdda19 // indirect
+	github.com/wasilibs/go-re2 v1.3.0 // indirect
 	github.com/xlab/treeprint v1.1.0 // indirect
 	github.com/xtaci/smux v1.5.16 // indirect
 	go.starlark.net v0.0.0-20200306205701-8dd3e2ee1dd5 // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/exp v0.0.0-20230724220655-d98519c11495 // indirect
 	golang.org/x/net v0.3.1-0.20221206200815-1e63c2f08a10 // indirect
 	golang.org/x/oauth2 v0.0.0-20220223155221-ee480838109b // indirect
 	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
-	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/term v0.3.0 // indirect
-	golang.org/x/text v0.5.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
 	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
